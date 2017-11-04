7f705301f9a19947116d3e2be812c45bf2453208
Maxime TRUDEL <mtrudel@e1r7p9.42.fr>
2017-11-04 16:30:27 +0100

    libft save correct

---
+++ ft_strsub.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtrudel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/04 16:28:30 by mtrudel           #+#    #+#             */
+/*   Updated: 2017/11/04 16:28:37 by mtrudel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*dest;
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	dest = (char*)malloc((len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = s[start];
+		i++;
+		start++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
